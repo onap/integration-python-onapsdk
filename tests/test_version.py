75b7c9be8db073ec728282d4027d5eb1f0a24802
Michal Jagiello <michal.jagiello@t-mobile.pl>
2023-03-06 14:34:20 +0000

    Release 10.4.0 version
    Release version with:
    - adding VSP archive
    - using k8s plugin without MSB
    
    Issue-ID: INT-2193
    Signed-off-by: Michal Jagiello <michal.jagiello@t-mobile.pl>
    Change-Id: Ica8059a2b34654cbb444f041c8bc67f34d92f797

--- tests/test_version.py
+++ tests/test_version.py
@@ -17,4 +17,4 @@ import onapsdk.version as version
 
 def test_version():
   """Check version is the right one."""
-  assert version.__version__ == '10.3.2'
+  assert version.__version__ == '10.4.0'

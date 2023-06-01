a384f479fb5270e72b3aeaa5c21aea3d827dc4a2
Alexey Aleynikov <alexey.aleynikov@telekom.com>
2023-06-01 19:02:04 +0300

    added changes in cps test
    Issue-ID: TEST-395
    Signed-off-by: Alexey Aleynikov <alexey.aleynikov@telekom.com>
    Change-Id: Ia1d22e52ca35aa3965eec5ee7acad1278bdfa679

--- tests/test_cps.py
+++ tests/test_cps.py
@@ -53,7 +53,7 @@ DATASPACE_SCHEMA_SET = {
 def test_dataspace():
     ds = Dataspace(name="test_ds")
     assert ds.name == "test_ds"
-    assert f"cps/api/v1/dataspaces/{ds.name}" in ds.url
+    assert f"cps/api/v2/dataspaces/{ds.name}" in ds.url
 
 @mock.patch("onapsdk.cps.Dataspace.send_message")
 def test_dataspace_create_anchor(mock_send_message):
@@ -88,6 +88,13 @@ def test_dataspace_get_anchor(mock_send_message_json):
     assert anchor.name == "anchor1"
     assert anchor.schema_set.name == "schemaSet1"
     assert anchor.schema_set.dataspace == ds
+
+@mock.patch("onapsdk.cps.Dataspace.send_message_json")
+def test_dataspace_get_dataspace(mock_send_message_json):
+    mock_send_message_json.return_value = {"name": "test_ds"}
+    dataspace_name = "test_ds"
+    dataspace = Dataspace.get_dataspace(dataspace_name)
+    assert dataspace.name == dataspace_name
 
 @mock.patch("onapsdk.cps.Dataspace.send_message_json")
 def test_dataspace_get_schema_set(mock_send_message_json):

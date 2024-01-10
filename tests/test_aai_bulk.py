acb593fd2e6d6c3c3f70b449538ac8e79e5c4d45
Michal Jagiello <michal.jagiello@t-mobile.pl>
2024-01-10 14:16:29 +0100

    Refactor bulk class
    Find failing requests and retry bulk without them
    
    Issue-ID: TEST-404
    Change-Id: Ia60f7761bcee99bcb6d0af0381db267d259e77a2
    Signed-off-by: Michal Jagiello <michal.jagiello@t-mobile.pl>

--- tests/test_aai_bulk.py
+++ tests/test_aai_bulk.py
@@ -38,7 +38,7 @@ BULK_RESPONSES = {
 def test_aai_bulk(mock_send_message_json):
     assert AaiBulk().url.endswith("bulk")
     mock_send_message_json.return_value = BULK_RESPONSES
-    responses = list(AaiBulk.single_transaction(
+    responses = list(AaiBulk().single_transaction(
         [
             AaiBulkRequest(
                 action="post",
@@ -65,7 +65,7 @@ def test_aai_bulk(mock_send_message_json):
 
     # Check if requests was splitted into chunks for generator
     mock_send_message_json.reset_mock()
-    responses = list(AaiBulk.single_transaction(
+    responses = list(AaiBulk().single_transaction(
         (
             AaiBulkRequest(
                 action="post",
@@ -78,7 +78,7 @@ def test_aai_bulk(mock_send_message_json):
 
     # Check if requests was splitted into chunks for list
     mock_send_message_json.reset_mock()
-    responses = list(AaiBulk.single_transaction(
+    responses = list(AaiBulk().single_transaction(
         [
             AaiBulkRequest(
                 action="post",
@@ -88,3 +88,27 @@ def test_aai_bulk(mock_send_message_json):
         ]
     ))
     assert mock_send_message_json.call_count == 2
+
+    mock_send_message_json.reset_mock()
+    responses = list(AaiBulk().single_transaction([]))
+    assert mock_send_message_json.call_count == 0
+
+
+def test_get_failed_operation_index():
+    aai_bulk = AaiBulk()
+    def get_formatted_response_error_with_operation(index: int) -> str:
+        return ('{"requestError":{"serviceException":{"messageId":"SVC3000","text":"Invalid input performing %1 on %2 (msg=%3)'
+                f' (ec=%4)","variables":["POST","v27/bulk/single-transaction","Invalid input performing %1 on %2:Error with operation {index}: '
+                'Missing required property: physical-location-type,Missing required property: street1,Missing required property: city,'
+                'Missing required property: postal-code,Missing required property: country,Missing required property: region","ERR.5.2.3000"]}}}')
+    def get_formatted_response_not_found(index: int) -> str:
+        return ('{"requestError":{"serviceException":{"messageId":"SVC3000","text":"Invalid input performing %1 on %2 (msg=%3) '
+                f'(ec=%4)","variables":["POST","v27/bulk/single-transaction","Invalid input performing %1 on %2:Operation {index} with action '
+                '(DELETE) on uri (/cloud-infrastructure/complexes/complex/test-parse-bulk-response) failed with status code (404), error code '
+                '(ERR.5.4.6114) and msg (Node Not Found:No Node of type complex found at: /cloud-infrastructure/complexes/complex/test-parse-bulk-response)","ERR.5.2.3000"]}}}')
+    assert aai_bulk._get_failed_operation_index(None) == -1
+    assert aai_bulk._get_failed_operation_index("Something on what there is no index") == -1
+    assert aai_bulk._get_failed_operation_index("There is an index: 0 but it's not a valid string") == -1
+    for i in [pow(10, x) for x in range(6)]:
+        assert aai_bulk._get_failed_operation_index(get_formatted_response_error_with_operation(i)) == i
+        assert aai_bulk._get_failed_operation_index(get_formatted_response_not_found(i)) == i
